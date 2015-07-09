03ede77e7bba3549af0e3dc7c724b58bb05df486
kevinhale-wf <kevin.hale@workiva.com>
2015-07-09 09:43:19 -0600

    new repo for learng js

---
+++ repo_deps.js
@@ -0,0 +1,14 @@
+//console.log(wtable1.json);
+           
+var data = {"success":true,"input_data":{"quantity-row_122":"1","price-row_122":" 35.1 "}};
+
+console.log(JSON.stringify(data.input_data["price-row_122"]));
+
+//load external json file
+
+
+//iterate through the objects 
+
+    //find the repo names 
+
+        //and their associated extended internal and external dependency names.
