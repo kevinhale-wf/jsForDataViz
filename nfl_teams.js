85849e848001c94abfac320627ca80d0169e4376
kevinhale-wf <kevin.hale@workiva.com>
2015-07-13 12:42:42 -0600

    using nested json

--- nfl_teams.js
+++ nfl_teams.js
@@ -1,10 +1,15 @@
 //console.log(wtable1.json);
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
            
-var data = {"success":true,"input_data":{"quantity-row_122":"1","price-row_122":" 35.1 "}};
+//var data = {"success":true,"input_data":{"quantity-row_122":"1","price-row_122":" 35.1 "}};
 
-console.log(JSON.stringify(data.input_data["price-row_122"]));
+//console.log(JSON.stringify(data.input_data["price-row_122"]));
 
-//load external json file
+//load external json file and log to concole
+$.getJSON("nfl_nested.json", function(json) {
+    console.log(json); 
+// this 
 
 
 //iterate through the objects 
